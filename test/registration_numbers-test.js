3467e4aeebd0a2743aeae02d44fda1a36f1e0cb0
Mercyfulll <mercyful.buthelezi@gmail.com>
2023-06-01 14:54:12 +0200

    Second commit

--- test/registration_numbers-test.js
+++ test/registration_numbers-test.js
@@ -9,20 +9,27 @@ describe("The registrationNumber test",function(){
 })
 describe("The regByTown test",function(){
 
-    it("It has to return 'CAG' or 'CAW' if town is 'george'", function (){
+    it("It has to return 'CX' if town is 'knysna'", function (){
         let regNumb = registrationNumber();
-        regNumb.regByTown('george');
+        regNumb.regByTown('knysna');
 
-        assert.equal("CAG",regNumb.regByTown('george'))
+        assert.equal("CX",regNumb.regByTown('knysna'))
     })
+    it("It has to return 'CA' if town is capetown", function (){
+        let regNumb = registrationNumber();
+        regNumb.regByTown('capetown');
+
+        assert.equal("CA",regNumb.regByTown('capetown'))
+    })
+
 })
 
-describe("The limitTown test",function(){
+describe("The regPlates test",function(){
 
-    it("It should return error message if registration is not from listed towns", function (){
+    it("It should store and return apprpriate registration numbers in an array", function (){
         let regNumb = registrationNumber();
-        regNumb.limitTown('GP 123');
+        regNumb.regPlates('Ca 1234');
 
-        assert.equal("Only enter registration from Paarl, Stellenbosch,Cape Town and George",regNumb.limitTown())
+        assert.equal("Ca 1234",regNumb.getPlates())
     })
 })
